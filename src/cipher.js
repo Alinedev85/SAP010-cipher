7548871c0f4677a40548d1e52c7d7a958b4d56ce
Aline Commit <128093293+Alinedev85@users.noreply.github.com>
2023-04-28 09:27:29 -0300

    projeto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,57 @@
 const cipher = {
-  // ...
-};
 
-export default cipher;
+  /* Codigo alfab.(0 a 25) /codigo ASCII maiuscula (65 até 90)/ codigo ASCII minúsculas(97 até 122)/ deslocar/ giro/ codigo alfb.*/
+  encode: function (chave, texto) {
+    if (typeof chave !== 'number' || typeof texto !== 'string') { //verificar se o tipo de dado armazenado não é numero
+      throw new TypeError("apenas string e numero");
+
+    } else {
+
+      let encodedString = "";
+      for (let i = 0; i < texto.length; i++) {
+        let caractere = texto[i];
+
+        if (caractere.match(/[A-Z]/i)) { //maiúscula, match=comparação 
+          const codigo = texto.charCodeAt(i);
+          caractere = String.fromCharCode(((codigo + 65 + chave) % 26) + 65);
+        } else if (caractere.match(/[a-z]/i)) {
+          const codigo = texto.charCodeAt(i);
+          caractere = String.fromCharCode(((codigo + 97 + chave) % 26) + 97);
+        }
+
+        encodedString += caractere;
+      }
+
+      return encodedString;
+    }
+  },
+
+  decode: function (chave, texto) {
+    if (typeof chave !== 'number' || typeof texto !== 'string') {
+      throw new TypeError("apenas string e numero");
+
+    } else {
+
+      let decodificadodString = "";
+      for (let i = 0; i < texto.length; i++) {
+        let caractere = texto[i];
+
+        if (caractere.match(/[A-Z]/i)) {
+          const codigo = texto.charCodeAt(i);
+          caractere = String.fromCharCode(((codigo + 65 - chave + 26) % 26) + 65);
+        } else if (caractere.match(/[a-z]/i)) {
+          const codigo = texto.charCodeAt(i);
+          caractere = String.fromCharCode(((codigo - 97 - chave + 26) % 26) + 97);
+        }
+
+        decodificadodString += caractere;
+      }
+
+      return decodificadodString;
+
+    }
+
+
+  },
+};
+export default cipher
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import cipher from './cipher.js';
+import cipher from './cipher';
 
-console.log(cipher);
+//                     ID do html          funcionar ao clicar        
+document.getElementById("bcifrar").addEventListener("click", cifrar); //executar a função
+document.getElementById("bdecifrar").addEventListener("click", decifrar);
+
+//console.log(cipher);
+
+function cifrar() { //declarando a funão 
+
+  const mensagem = document.getElementById("mensagem").value; /* cria uma variavel e atribui o valor fornecido no campo de input*/
+  const chave = parseInt(document.getElementById("chave"));
+  const codificado = cipher.encode(mensagem, chave);
+  document.getElementById("resultado").value = codificado;
+}
+
+function decifrar() {
+
+  const mensagem = document.getElementById("mensagem").value; /* cria uma variavel e atribui o valor fornecido no campo de input*/
+  const chave = parseInt(document.getElementById("chave")); /* parseInt- transforma o valor recebido do input (string) em um num. inteiro*/
+  const codificado = cipher.decode(mensagem, chave); /*será usada para armazenar msg cifrada*/
+  document.getElementById("resultado").value = codificado;
+}
+
+
+
+
