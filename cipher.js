a33d94642b02712b39e1d6ed1ebf447c3078f2ae
Aline Commit <128093293+Alinedev85@users.noreply.github.com>
2023-04-28 10:09:06 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,58 @@
+const cipher = {
+
+  /* Codigo alfab.(0 a 25) /codigo ASCII maiuscula (65 até 90)/ codigo ASCII minúsculas(97 até 122)/ deslocar/ giro/ codigo alfb.*/
+  encode: function (chave, texto) {
+    if (typeof chave !== 'number' || typeof texto !== 'string') { //verificar se o tipo de dado armazenado não é numero
+      throw new TypeError("string e numero");
+
+    } else {
+
+      let encodedString = "";
+      for (let i = 0; i < texto.length; i++) {
+        let caractere = texto[i];
+
+        if (caractere.match(/[A-Z]/i)) { //maiúscula, match=comparação 
+          const codigo = texto.charCodeAt(i);
+          caractere = String.fromCharCode(((codigo + 65 + chave) % 26) + 65);/* se vdd, ASCII - 65= letra alfabeto o resultado usa o operador modulo(%) para que esteja dentro do alfabeto*/
+        } else if (caractere.match(/[a-z]/i)) {
+          const codigo = texto.charCodeAt(i);
+          caractere = String.fromCharCode(((codigo + 97 + chave) % 26) + 97);
+        }
+
+        encodedString += caractere;
+      }
+
+      return encodedString;
+    }
+  },
+
+  decode: function (chave, texto) {
+    if (typeof chave !== 'number' || typeof texto !== 'string') {
+      throw new TypeError("apenas string e numero");
+
+    } else {
+
+      let decodificadodString = "";
+      for (let i = 0; i < texto.length; i++) {
+        let caractere = texto[i];
+
+        if (caractere.match(/[A-Z]/i)) {
+          const codigo = texto.charCodeAt(i);
+          if (codigo >= 65 && codigo <= 90);
+            caractere = String.fromCharCode(((codigo + 65 - chave + 26) % 26) + 65);
+        } else if (caractere.match(/[a-z]/i)) {
+          if (codigo >= 97 && codigo <= 122);
+            caractere = String.fromCharCode(((codigo - 97 - chave + 26) % 26) + 97);
+        }
+
+        decodificadodString += caractere;
+      }
+
+      return decodificadodString;
+
+    }
+
+
+  },
+};
+export default cipher
