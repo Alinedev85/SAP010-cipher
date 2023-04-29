4930778c4394b3950ed5cd69c047dd9ee9eb84b1
Aline Commit <128093293+Alinedev85@users.noreply.github.com>
2023-04-29 15:29:21 -0300

    criação

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,23 @@
+console.log ("cipher")
 const cipher = {
 
   /* Codigo alfab.(0 a 25) /codigo ASCII maiuscula (65 até 90)/ codigo ASCII minúsculas(97 até 122)/ deslocar/ giro/ codigo alfb.*/
   encode: function (chave, texto) {
     if (typeof chave !== 'number' || typeof texto !== 'string') { //verificar se o tipo de dado armazenado não é numero
-      throw new TypeError("apenas string e numero");
+      throw new TypeError("string e numero");
 
     } else {
 
-      let encodedString = "";
-      for (let i = 0; i < texto.length; i++) {
-        let caractere = texto[i];
-
-        if (caractere.match(/[A-Z]/i)) { //maiúscula, match=comparação 
-          const codigo = texto.charCodeAt(i);
-          caractere = String.fromCharCode(((codigo + 65 + chave) % 26) + 65);
-        } else if (caractere.match(/[a-z]/i)) {
-          const codigo = texto.charCodeAt(i);
-          caractere = String.fromCharCode(((codigo + 97 + chave) % 26) + 97);
+      let encodedString = ""; //variável vazia para armazenar o texto cript.
+      for (let i = 0; i < texto.length; i++) { // loop
+        let caractere = texto[i]; // armazenar o caractere atual do texto original.
+        const codigo = texto.charCodeAt(i); //variável para o valor numérico do caractere atual do texto original ----- tabela ASCII.
+        if (caractere.match(/[A-Z]/)) { //maiúscula, match=comparação 
+          if (codigo >= 65 && codigo <= 90); // valor numérico do caractere atual está dentro do intervalo  
+          caractere = String.fromCharCode(((codigo - 65 + chave) % 26) + 65);//se for miniscula calcula o valor correspondente a letra codificada +chave fixa 
+        } else if (caractere.match(/[a-z]/)) { //se o caractere atual não for uma letra maiúscula, o código verifica se é uma letra minúscula
+          if (codigo >= 97 && codigo <= 122); //valor numérico do caractere atual está dentro do intervalo correspondente às letras minúsculas na tabela ASCII.
+          caractere = String.fromCharCode(((codigo - 97 + chave) % 26) + 97);
         }
 
         encodedString += caractere;
@@ -35,13 +36,20 @@ const cipher = {
       let decodificadodString = "";
       for (let i = 0; i < texto.length; i++) {
         let caractere = texto[i];
+        const codigo = texto.charCodeAt(i);
+        if (caractere.match(/[A-Z]/)) {
+          if (codigo >= 65 && codigo <= 90) {
+            caractere = String.fromCharCode(((codigo + 65 - chave + 26) % 26) + 65);
+          }
+        } else if (caractere.match(/[a-z]/)) {
+          if (codigo >= 97 && codigo <= 122) {
+            let resultado = codigo - 97 - chave;
+            while (resultado < 0) {  //laço adicional de 26
+              resultado += 26;
+            }
+            caractere = String.fromCharCode((resultado % 26) + 97);
 
-        if (caractere.match(/[A-Z]/i)) {
-          const codigo = texto.charCodeAt(i);
-          caractere = String.fromCharCode(((codigo + 65 - chave + 26) % 26) + 65);
-        } else if (caractere.match(/[a-z]/i)) {
-          const codigo = texto.charCodeAt(i);
-          caractere = String.fromCharCode(((codigo - 97 - chave + 26) % 26) + 97);
+          }
         }
 
         decodificadodString += caractere;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-import cipher from './cipher';
+import cipher from "./cipher.js";
 
-//                     ID do html          funcionar ao clicar        
-document.getElementById("bcifrar").addEventListener("click", cifrar); //executar a função
-document.getElementById("bdecifrar").addEventListener("click", decifrar);
-
-//console.log(cipher);
-
-function cifrar() { //declarando a funão 
-
+function cifrar() { //declarando a função 
+ 
   const mensagem = document.getElementById("mensagem").value; /* cria uma variavel e atribui o valor fornecido no campo de input*/
-  const chave = parseInt(document.getElementById("chave"));
-  const codificado = cipher.encode(mensagem, chave);
+  const chave = parseInt(document.getElementById("chave").value);
+  const codificado = cipher.encode(chave,mensagem);
   document.getElementById("resultado").value = codificado;
 }
 
 function decifrar() {
 
   const mensagem = document.getElementById("mensagem").value; /* cria uma variavel e atribui o valor fornecido no campo de input*/
-  const chave = parseInt(document.getElementById("chave")); /* parseInt- transforma o valor recebido do input (string) em um num. inteiro*/
-  const codificado = cipher.decode(mensagem, chave); /*será usada para armazenar msg cifrada*/
-  document.getElementById("resultado").value = codificado;
+  const chave = parseInt(document.getElementById("chave").value); /* parseInt- transforma o valor recebido do input (string) em um num. inteiro*/
+  const decodificado = cipher.decode(chave,mensagem); /*será usada para armazenar msg cifrada*/
+  document.getElementById("resultado").value = decodificado;
 }
 
 
+//                     ID do html          funcionar ao clicar        
+document.getElementById("bcifrar").addEventListener("click", cifrar); //executar a função
+document.getElementById("bdecifrar").addEventListener("click", decifrar);
 
+//console.log(cipher);
 
